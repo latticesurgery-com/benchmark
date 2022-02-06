359217c88a52c40352fc19a0694f81cc588cbdeb
George Watkins <gwwatkin@sfu.ca>
2022-02-06 00:26:34 -0800

    Refactor benchmarking in the QFT

--- qft.py
+++ qft.py
@@ -1,10 +1,7 @@
 
 
 import cProfile
-import io
-import os
-import pstats
-import sys
+
 
 import lsqecc.pipeline.lattice_surgery_compilation_pipeline as lscp
 
@@ -21,8 +18,18 @@ if __name__=="__main__":
 
     qft:qiskit.QuantumCircuit = qft_benchmark.qft_gate(3)
 
+    print("QASM input:")
+    print(qkvis.circuit_drawer(qft).single_string())
+    print("Decomposed QASM input:")
+    decompose_qft = qft.decompose()
+    print(qkvis.circuit_drawer(decompose_qft).single_string())
+    print("Decomposed raw QASM input:")
+    print(decompose_qft.qasm())
+    print("Decomposed raw QASM input (formatted):")
+    print(decompose_qft.qasm(formatted=True))
+
     with cProfile.Profile() as profiler:
-        slices, text = lscp.compile_str(qft.qasm(),
+        slices, text = lscp.compile_str(decompose_qft.qasm(),
                                         simulation_type=lscp.lssim.SimulatorType.NOOP,
                                         apply_litinski_transform=False)
 
