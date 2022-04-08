9c9c6fc931690df0848250ed97d9f09bf650fea4
George Watkins <gwwatkin@sfu.ca>
2022-04-08 05:36:21 -0700

    Add start of hamiltonian simulation

---
+++ hamiltonian_sim/hamiltonian_sim.py
@@ -0,0 +1,11 @@
+
+from app_oriented_benchmarks_adapted.hamiltonian_simulation_benchmark import HamiltonianSimulation
+
+"""
+EXAMPLE parameters for HamiltonianSimulation() (collected from the AOB runs):
+
+"""
+
+
+if __name__ == "__main__":
+    HamiltonianSimulation()
