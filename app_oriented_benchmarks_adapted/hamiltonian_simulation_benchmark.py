7d4a7639b7887282f0f14c0f52ff7d5eb86094c4
George Watkins <gwwatkin@sfu.ca>
2022-04-08 03:02:32 -0700

    Replace approximations with pi

--- app_oriented_benchmarks_adapted/hamiltonian_simulation_benchmark.py
+++ app_oriented_benchmarks_adapted/hamiltonian_simulation_benchmark.py
@@ -121,7 +121,7 @@ def xx_gate(tau):
     qc.h(qr[0])
     qc.h(qr[1])
     qc.cx(qr[0], qr[1])
-    qc.rz(3.1416 * tau, qr[1])
+    qc.rz(np.pi * tau, qr[1])
     qc.cx(qr[0], qr[1])
     qc.h(qr[0])
     qc.h(qr[1])
@@ -142,7 +142,7 @@ def yy_gate(tau):
     qc.h(qr[0])
     qc.h(qr[1])
     qc.cx(qr[0], qr[1])
-    qc.rz(3.1416 * tau, qr[1])
+    qc.rz(np.pi * tau, qr[1])
     qc.cx(qr[0], qr[1])
     qc.h(qr[0])
     qc.h(qr[1])
@@ -161,7 +161,7 @@ def zz_gate(tau):
     qr = QuantumRegister(2);
     qc = QuantumCircuit(qr, name="zz_gate")
     qc.cx(qr[0], qr[1])
-    qc.rz(3.1416 * tau, qr[1])
+    qc.rz(np.pi * tau, qr[1])
     qc.cx(qr[0], qr[1])
 
     # save circuit example for display
@@ -178,14 +178,14 @@ def xxyyzz_opt_gate(tau):
     gamma = tau
     qr = QuantumRegister(2);
     qc = QuantumCircuit(qr, name="xxyyzz_opt")
-    qc.rz(3.1416 / 2, qr[1])
+    qc.rz(np.pi / 2, qr[1])
     qc.cx(qr[1], qr[0])
-    qc.rz(3.1416 * gamma - 3.1416 / 2, qr[0])
-    qc.ry(3.1416 / 2 - 3.1416 * alpha, qr[1])
+    qc.rz(np.pi * gamma - np.pi / 2, qr[0])
+    qc.ry(np.pi / 2 - np.pi * alpha, qr[1])
     qc.cx(qr[0], qr[1])
-    qc.ry(3.1416 * beta - 3.1416 / 2, qr[1])
+    qc.ry(np.pi * beta - np.pi / 2, qr[1])
     qc.cx(qr[1], qr[0])
-    qc.rz(-3.1416 / 2, qr[0])
+    qc.rz(-np.pi / 2, qr[0])
 
     # save circuit example for display
     global XXYYZZ_
