f0849ef80534f6a1c2c9486fe37655dc23c5ada6
Varun Seshadri <varun.seshadri@tum.de>
2023-02-12 19:34:10 +0100

    - bugfixes in grover's benchmarks

--- grover/grover_benchmark_suite.py
+++ grover/grover_benchmark_suite.py
@@ -10,11 +10,11 @@ PYTHON_EXECUTABLE = "/home/varunseshadri/.pyenv/versions/env-lsqecc/bin/python"
 if __name__ == "__main__":
 
     print("qubit_num, ls_instruction_count,slices_count,slicing_time_s, total_time_s")
-    for qubit_num in [5,6,7, 8, 9, 10]:
+    for qubit_num in [4]:
         start = time.time()
         command_output = subprocess.check_output(
-            f" {PYTHON_EXECUTABLE} {GENERATOR_EXECUTABLE} {qubit_num}", shell=True).decode('utf-8')
+            f" {PYTHON_EXECUTABLE} {GENERATOR_EXECUTABLE} {qubit_num} | {SLICER_EXECUTABLE} -o -f progress -l grover/grover_l.txt", shell=True).decode('utf-8')
         command_output = command_output[:-1] # remove trailing newline
         print(f"({qubit_num}, {command_output}, {time.time() - start}),")
 
-        #  | {SLICER_EXECUTABLE} -o -f progress -l grover/grover_l.txt
+        #
